fa82fcfa5b750145250e960874c33e5fd2d71eeb
Nikhil Dubbaka <info@technikh.com>
2014-11-17 23:06:19 -0500

    adding git fields and testing server storing & reading data.

--- repatcha/repatcha/static/js/src/repatcha.js
+++ repatcha/repatcha/static/js/src/repatcha.js
@@ -1,13 +1,19 @@
 /* Javascript for RePatchaXBlock. */
 function RePatchaXBlock(runtime, element) {
 
-
-  /*capturing html element where 3d model will be drawn*/
-  var base =$('.repatcha_block',element).get(0);
-  var diff_html = "1 diff --git a/tests/behat/behat.yml b/tests/behat/behat.yml";
-  $('.prettyprint', element).html(diff_html);
-  $('.prettyprint', element)
-	.annotator('setupPlugins');
-  //prettyPrint();
-
+	/* Ajax call that gets git repatcha data from the server */
+	$.ajax({
+	    type: "POST",
+	    url: runtime.handlerUrl(element, 'send_Data'),
+	    data: JSON.stringify({requested: true}),
+	    success: function(result) {
+	      git_owner = JSON.parse(result.git_owner);
+  
+		  var diff_html = git_owner + " 2 diff --git a/tests/behat/behat.yml b/tests/behat/behat.yml";
+		  $('.prettyprint', element).html(diff_html);
+		  $('.prettyprint', element)
+			.annotator('setupPlugins');
+		  //prettyPrint();
+	    }
+    });  
 }
--- repatcha/repatcha/static/js/src/repatcha_edit.js
+++ repatcha/repatcha/static/js/src/repatcha_edit.js
@@ -23,11 +23,9 @@ function RePatchaXBlockEdit(runtime, element) {
     var el = $(element);
     var data = {
       word: el.find('input[name=word]').val(),
-      location: el.find('input[id=loc]').val(),
-      backgnd: el.find('input[id=bg1]').val(),
-      backgnd1: el.find('input[id=bg2]').val(),
-      height: el.find('input[id=vheight]').val(),
-      width: el.find('input[id=vwidth]').val()
+      git_owner: el.find('input[id=git_owner]').val(),
+      git_repo: el.find('input[id=git_repo]').val(),
+      git_pr_number: el.find('input[id=git_pr_number]').val()
     };
 
     $.post(handlerUrl, JSON.stringify(data)).done(function(response) {
@@ -37,8 +35,8 @@ function RePatchaXBlockEdit(runtime, element) {
   });
 
   /*Functions for validation of input for background color 1 and 2*/
-  $(element).on('keyup','input#bg1',function(){
-    validateColor(element, '1', this);
+  $(element).on('keyup','input#git_owner',function(){
+    //validateColor(element, '1', this);
   });
 
  /* Function for canceling  */
