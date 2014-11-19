55a49cea57da2909e91b9b0fa071192d62638edc
ndubbaka <info@technikh.com>
2014-11-19 01:17:42 +0000

    working variable store & retrieve.

--- repatcha/repatcha/static/js/src/repatcha.js
+++ repatcha/repatcha/static/js/src/repatcha.js
@@ -1,19 +1,12 @@
 /* Javascript for RePatchaXBlock. */
 function RePatchaXBlock(runtime, element) {
-
-	/* Ajax call that gets git repatcha data from the server */
-	$.ajax({
-	    type: "POST",
-	    url: runtime.handlerUrl(element, 'send_Data'),
-	    data: JSON.stringify({requested: true}),
-	    success: function(result) {
-	      git_owner = JSON.parse(result.git_owner);
+/*handler for Ajax call*/
+  var dFile=runtime.handlerUrl(element,'upload_objfile');
+	      git_owner = $(".test").data("git-owner");
   
 		  var diff_html = git_owner + " 2 diff --git a/tests/behat/behat.yml b/tests/behat/behat.yml";
 		  $('.prettyprint', element).html(diff_html);
 		  $('.prettyprint', element)
 			.annotator('setupPlugins');
 		  //prettyPrint();
-	    }
-    });  
 }
